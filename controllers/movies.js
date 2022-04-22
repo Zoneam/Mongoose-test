09f34cb0884d9d62b328f1a7dcc8235228c4afea
Zoneam <zoneam@gmail.com>
2022-04-22 13:46:40 -0700

    working form

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,27 @@
+const Movie = require('../models/movie')
+
 module.exports = {
-    new: newMovie
+    new: newMovie,
+    create
   };
 
 function newMovie(req, res) {
     res.render('movies/new');
+}
+
+function create(req, res) {
+// convert nowShowing's checkbox of nothing or "on" to boolean
+req.body.nowShowing = !!req.body.nowShowing;
+// remove any whitespace at start and end of cast
+req.body.cast = req.body.cast.trim();
+// split cast into an array if it's not an empty string - using a regular expression as a separator
+if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+const movie = new Movie(req.body);
+movie.save(function(err) {
+  // one way to handle errors
+  if (err) return res.render('movies/new');
+  console.log(movie);
+  // for now, redirect right back to new.ejs
+  res.redirect('/movies/new');
+});
 }
--- routes/movies.js
+++ routes/movies.js
@@ -5,4 +5,10 @@ const movieCtrl = require('../controllers/movies')
 // if user visits this route fire controller
 router.get('/new', movieCtrl.new)
 
+
+// to process submit button on form 
+
+router.post('/', movieCtrl.create);
+
+
 module.exports = router;
